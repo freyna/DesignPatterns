dd950a338e767149a25e2d6d50dc9924030372aa
Filiberto Reyna <ing.filiberto.reyna@gmail.com>
2022-07-13 13:25:23 -0500

    Proyecto api rest para singleton

---
+++ DesignPattern/DesignPatternASP/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tools;
+
+namespace DesignPatternASP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("GetProviders/{id}")]
+        public async Task<IActionResult> GetProviders([FromQuery] int id) 
+        {
+            await Log.GetInstance("log.txt").Save("Entrando a GetProviders");
+
+            return Ok();
+        }
+
+        [HttpGet("GetUsers/{url}")]
+        public async Task<IActionResult> GetUsers([FromQuery] string url)
+        {
+            await Log.GetInstance("log.txt").Save("Entrando a GetUsers");
+
+            return Ok();
+        }
+
+    }
+}
