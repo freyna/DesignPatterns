270afb975a680060a9c82b06ff26e3b47127c1d4
Filiberto Reyna <ing.filiberto.reyna@gmail.com>
2022-07-13 13:24:59 -0500

    Se agrega funcionalidad para log con singleton

--- DesignPattern/Tools/Log.cs
+++ DesignPattern/Tools/Log.cs
@@ -1,6 +1,28 @@
 ﻿namespace Tools
 {
-    public class Log
+    public sealed class Log
     {
+        private static Log instance = null;
+        private string Path;
+
+        public static Log GetInstance (string path)
+        {
+            if(instance == null)
+            {
+                instance = new Log (path);
+            }
+
+            return instance;
+        }
+
+        private Log(string path)
+        {
+            Path = path;
+        }
+
+        public async Task Save(string message)
+        {
+            await File.AppendAllTextAsync(Path, message + Environment.NewLine);
+        }
     }
 }
