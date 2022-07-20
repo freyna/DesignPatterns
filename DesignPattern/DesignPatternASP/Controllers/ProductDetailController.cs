13f3647e4176da0e8d1bd775d23ab8de8a826916
Filiberto Reyna <ing.filiberto.reyna@gmail.com>
2022-07-20 09:01:27 -0500

    Implementación de Factory Method

---
+++ DesignPattern/DesignPatternASP/Controllers/ProductDetailController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tools.Earn;
+
+namespace DesignPatternASP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductDetailController : ControllerBase
+    {
+        public IActionResult Index(decimal total)
+        {
+            LocalEarnFactory localEarnFactory = new LocalEarnFactory(0.20m);
+            ForeignEarnFactory foreignEarnFactory = new ForeignEarnFactory(0.30m, 20);
+
+            var localEarn = localEarnFactory.GetEarn();
+            var foreignEarn = foreignEarnFactory.GetEarn();
+
+            var totalLocal = total + localEarn.Earn(total);
+            var totalForeign = total + foreignEarn.Earn(total);
+
+            var totalPrint = $"Total local: {totalLocal}\n Total Foreign: {totalForeign}";
+
+            return Ok(totalPrint);
+        }
+    }
+}
---
+++ DesignPattern/Tools/Earn/IEarn.cs
@@ -0,0 +1,7 @@
+﻿namespace Tools.Earn
+{
+    public interface IEarn
+    {
+        public decimal Earn(decimal amount);
+    }
+}
