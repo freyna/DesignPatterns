9587955144116c3ad5cbfe5a62b813d76d3b47b8
Filiberto Reyna <ing.filiberto.reyna@gmail.com>
2022-08-04 13:05:58 -0500

    Se agrega funcionalidad para explicación de repository

--- DesignPattern/DesignPattern/Program.cs
+++ DesignPattern/DesignPattern/Program.cs
@@ -1,7 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DesignPattern._01Singleton;
+using DesignPattern.Models;
+using DesignPattern.RepositoryPattern;
 
 var log = Log.Instance;
 
 await log.Save("a");
 await log.Save("b");
+
+using (var context = new DesignPatternContext())
+{
+    var repository = new Repository<Beer>(context);
+
+    var newBeer = new Beer
+    {
+        Name = "Otra cerveza 2",
+        Style = "Natural"
+    };
+
+    repository.Add(newBeer);
+
+    repository.Save();
+
+    var beers = repository.GetAll();
+
+    foreach(var beer in beers)
+    {
+        Console.WriteLine(beer.Name);
+    }
+ }
+
+
